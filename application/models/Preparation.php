db1476c422146e3fdaa62e7477f6395fe10d8ed1
Miguel <miguel2354@googlemail.com>
2012-06-09 15:15:27 +0200

    Datenbankzugriff in DatabaseAccess Klasse ausgelagert

--- application/models/Preparation.php
+++ application/models/Preparation.php
@@ -6,8 +6,9 @@
  * @author mi4o
  * @version 
  */
-
+include 'helpers/DatabaseAccess.php';
 require_once 'Zend/Db/Table/Abstract.php';
+
 class Preparation extends Zend_Db_Table_Abstract {
 	/**
 	 * The default table name
@@ -16,28 +17,16 @@ class Preparation extends Zend_Db_Table_Abstract {
 	
 	public function getPreparationList()
 	{
-		$db = Zend_Registry::get('dbAdapter');
-		$db->getConnection();
-	
 		$sql = 'SELECT lpr_reqid, lpr_status, lpr_udf8, lpr_udf17 FROM load_preparation';
-		$result = $db->query($sql);
-			
-		$db->closeConnection();
-	
-		return $result->fetchAll();
+
+		return DatabaseAccess::getResult($sql);
 	}
 	
 	public function getPreparationStatusCounts()
-	{
-		$db = Zend_Registry::get('dbAdapter');
-		$db->getConnection();
-	
+	{	
 		$sql = 'SELECT lpr_status, COUNT(*) as amount FROM load_preparation GROUP BY lpr_status;';
-		$result = $db->query($sql);
-			
-		$db->closeConnection();
 	
-		return $result->fetchAll();
+		return DatabaseAccess::getResult($sql);
 	}
 	
 }
---
+++ application/models/helpers/DatabaseAccess.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *
+ * @author mi4o
+ * @version 
+ */
+require_once 'Zend/Loader/PluginLoader.php';
+require_once 'Zend/Controller/Action/Helper/Abstract.php';
+
+/**
+ * DatabaseAccess Action Helper
+ *
+ * @uses actionHelper Zend_Controller_Action_Helper
+ */
+class DatabaseAccess {
+	
+	/**
+	 * Constructor
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+	}
+	
+	public function getResult($sql) {
+
+		$db = Zend_Registry::get('dbAdapter');
+		$db->getConnection();
+		
+		$result = $db->query($sql);
+			
+		$db->closeConnection();
+		
+		return $result->fetchAll();
+	}
+	
+}
